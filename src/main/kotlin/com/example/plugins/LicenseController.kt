77101996e9525fe2e364a3a66e8c80868bc903e5
Andrew Ma <ama25@belmontschools.net>
2024-08-14 17:58:06 -0400

    Create LicenseController.kt

---
+++ src/main/kotlin/com/example/plugins/LicenseController.kt
@@ -0,0 +1,23 @@
+package com.example.plugins
+
+import com.example.database.DBRepo
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.*
+import kotlinx.coroutines.*
+
+fun Application.configureLicenseController() {
+
+    install(ContentNegotiation) {
+        json()
+    }
+
+    routing {
+
+    }
+}

774741a37f1e5404123ef89106246a5b65b3fcf9
Andrew Ma <ama25@belmontschools.net>
2024-08-15 11:31:17 -0400

    Added create and delete endpoints starting code

--- src/main/kotlin/com/example/plugins/DevicesController.kt
+++ src/main/kotlin/com/example/plugins/DevicesController.kt
@@ -1,23 +1,84 @@
 package com.example.plugins
 
-import com.example.database.DBRepo
+import io.ktor.client.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.sql.*
-import kotlinx.coroutines.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+
+@Serializable
+data class LicenseResponse(val customerId: String, val licenses: List<License>)
+
+@Serializable
+data class License(val id: String, val expiration: Double, val used: Boolean)
+
+fun extractLicenses(licensesJson: String): List<License> {
+    val json = Json { ignoreUnknownKeys = true }
+    val licenseResponse = json.decodeFromString<LicenseResponse>(licensesJson)
+    return licenseResponse.licenses
+}
+
+object HttpClientSingleton {
+    val client: HttpClient by lazy {
+        HttpClient(CIO) {
+            install(Auth) {
+                basic {
+                    credentials {
+                        BasicAuthCredentials(username = "userName", password = "SuperSecretPassword1234!!")
+                    }
+                    realm = "Access to the '/' path"
+                }
+            }
+        }
+    }
+}
 
 fun Application.configureDevicesController() {
 
-    install(ContentNegotiation) {
-        json()
-    }
-
     routing {
+        post("/createDevice/{customerId}") {
+            val headers: Headers = call.request.headers
+            val customerId: String = call.parameters["customerId"] ?: throw IllegalArgumentException("Invalid customer ID")
+            val deviceName: String = headers["deviceName"] ?: throw IllegalArgumentException("Invalid device name")
+            val deviceId = headers["deviceId"] ?: throw IllegalArgumentException("Invalid device type")
+            val locationId: String = headers["locationId"] ?: throw IllegalArgumentException("Invalid location ID")
+            val licensesJson = getLicenses(customerId)
+            val extractedLicenses = extractLicenses(licensesJson)
+            println("Extracted Licenses: $extractedLicenses")
 
+            val unusedLicense = extractedLicenses.find { !it.used && it.expiration > System.currentTimeMillis() / 1000 }
+            if (unusedLicense != null) {
+                println("Unused License: $unusedLicense")
+                HttpClientSingleton.client.post("https://wbla-sandbox-rest.azurewebsites.net/license/${unusedLicense.id}/assign")
+                // set the license to used in DB
+                call.respond(HttpStatusCode.OK, "Device created for customer $customerId with device name $deviceName, device ID $deviceId and location ID $locationId using license ${unusedLicense.id}")
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "All licenses are used or expired. Remove devices or purchase more licenses.")
+            }
+        }
+        post("/deleteDevice/{customerId}") {
+
+            val headers: Headers = call.request.headers
+            // get the license id from db or headers
+            val licenseId: String = headers["licenseId"] ?: throw IllegalArgumentException("Invalid license ID")
+            val customerId: String = call.parameters["customerId"] ?: throw IllegalArgumentException("Invalid customer ID")
+            val deviceId: String = headers["deviceId"] ?: throw IllegalArgumentException("Invalid device ID")
+            HttpClientSingleton.client.post("https://wbla-sandbox-rest.azurewebsites.net/license/${licenseId}/unassign")
+        }
     }
+}
+
+suspend fun getLicenses(customerId: String): String {
+    // Change later to not create client every time
+
+    val response: HttpResponse = HttpClientSingleton.client.get("https://wbla-sandbox-rest.azurewebsites.net/license/$customerId")
+
+    return response.bodyAsText()
 }
